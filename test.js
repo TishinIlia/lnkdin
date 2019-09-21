e912f05a8b6c504a380af1f9bcc3045d05c4502f
Ilya Tishin <nenaviju4tverg@gmail.com>
2019-09-21 05:03:18 +0300

    init commit -m

---
+++ test.js
@@ -0,0 +1,15 @@
+const scrapedin = require('scrapedin');
+
+let profileScraper = undefined;
+let profile = undefined;
+
+scrapedin({ email: 'orloff.anya@gmail.com', password: 'poiuy0987' })
+	.then((data) => {
+		console.log(data);
+		profileScraper = data;
+		profileScraper('https://www.linkedin.com/in/yalefox')
+			.then((data) => {
+				profile = data;
+				console.log(profile);
+			});
+	});
